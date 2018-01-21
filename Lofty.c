6de1910c46cb1a67accf468108fc7233be1b8ef6
CBernard23 <clbernard2016@icloud.com>
2018-01-21 01:20:37 -0600

    Funny prints

--- Lofty.c
+++ Lofty.c
@@ -1,17 +1,43 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios og_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios);
+}
+
 void enableRawMode()
 {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &og_termios);
+    atexit(disableRawMode);
 
-    tcgetattr(STDIN_FILENO, &raw);
+    struct termios raw = og_termios;
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
 }
 
 int main()
 {
+    enableRawMode();
+
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\n");
+        } else {
+            printf("%d ('%c')\n");
+        }
+    }
     return 0;
 }
